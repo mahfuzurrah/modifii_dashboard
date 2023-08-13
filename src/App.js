32633413f0f2fc37ea49de1f37fca8757d673993
Mahfuzur Rahman <mahfuzuruiux@gmail.com>
2023-08-13 10:18:10 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LogIn from "./Pages/Login";
-import MainLayout from "./Components/Navbar/MainLayout";
-import Dashboard from "./Pages/Dashboard";
-import Contacts from "./Pages/Contacts";
-import Products from "./Pages/Products";
-import Support from "./Pages/Support";
-import UserManagement from "./Pages/UserManagement";
+// import MainLayout from "./Components/Navbar/MainLayout";
+// import Dashboard from "./Pages/Dashboard";
+// import Contacts from "./Pages/Contacts";
+// import Products from "./Pages/Products";
+// import Support from "./Pages/Support";
+// import UserManagement from "./Pages/UserManagement";
 
 
 function App() {
@@ -15,13 +15,13 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LogIn />} />
-          <Route path="/" element={<MainLayout/>}>
+          {/* <Route path="/" element={<MainLayout/>}>
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/products" element={<Products />} />
             <Route path="/support" element={<Support />} />
             <Route path="/user-management" element={<UserManagement />} />
-          </Route>
+          </Route> */}
         </Routes>
       </Router>
     </>
